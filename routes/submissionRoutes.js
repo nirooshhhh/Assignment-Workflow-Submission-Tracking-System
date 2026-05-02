a149111b1edea8efc7b81e2af2d49c3133722e53
nirooshhhh <nirosh2006@gmail.com>
2026-05-02 16:21:35 +0530

    complete

--- routes/submissionRoutes.js
+++ routes/submissionRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Submission = require("../models/Submission");
 
 const {
   submitAssignment,
@@ -11,5 +12,10 @@ router.post("/:assignmentId", submitAssignment);
 
 // Get submissions for assignment
 router.get("/:assignmentId", getSubmissionsByAssignment);
+
+router.get("/", async (req, res) => {
+  const submissions = await Submission.find();
+  res.json(submissions);
+});
 
 module.exports = router;
